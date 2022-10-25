b97bc08b2f51075fc2e5230312375b5afb3ab4b4
xuekelindun <xuekelindun@kuaishou.com>
2022-10-25 09:20:21 +0000

    zufang ok

--- HouseInfo/spiders/beike_zufang_simple.py
+++ HouseInfo/spiders/beike_zufang_simple.py
@@ -21,30 +21,32 @@ districtDic = {
     'yanqing': '延庆' 
 }
 
-class BeikeErshoufangSimpleSpider(scrapy.Spider):
-    name = 'beike-ershoufang-simple'
-    allowed_domains = ['bj.ke.com']
+beike_url = "https://bj.zu.ke.com/"
+base_url = "https://bj.zu.ke.com/zufang/"
+
+class BeikeZufangSimpleSpider(scrapy.Spider):
+    name = 'beike-zufang-simple'
+    allowed_domains = ['bj.zu.ke.com']
 
     custom_settings = {
         'DOWNLOAD_DELAY': '10',
     }
 
-
     def start_requests(self):
         for key in districtDic.keys():
-            url = 'https://bj.ke.com/ershoufang/{}'.format(key) + "/"
+            url = base_url + '{}'.format(key) + "/"
             yield scrapy.Request(url, self.districtPage)
             # TODO 删了
             # return
 
     def districtPage(self, response):
         self.logger.info("districtPage 城区:%s 开始爬取", response.url)
-        subDistrictResultList = response.css('dd div > a') 
+        subDistrictResultList = response.css('.filter__item--level3 > a') 
         if (len(subDistrictResultList) == 0):
             self.logger.error("districtPage %s 没有数据", response.url)
         for subDistrict in subDistrictResultList:
             urlPath = subDistrict.xpath("@href").extract_first()
-            if ("ershoufang" in urlPath):
+            if ("zufang" in urlPath):
                 isCity = False
                 for key in districtDic.keys():
                     isCity = (("/" + key + "/") in urlPath)
@@ -53,10 +55,10 @@ class BeikeErshoufangSimpleSpider(scrapy.Spider):
                 if not isCity:
                     subDistrict = urlPath.split("/")
                     try:
-                        subDistrictUrl = 'https://bj.ke.com/ershoufang/{}'.format(subDistrict[2]) + "/"
+                        subDistrictUrl = base_url + '{}'.format(subDistrict[2]) + "/"
                         yield scrapy.Request(subDistrictUrl, self.subDistrictPage)
                         # TODO 删了
-                        return
+                        # return
                     except Exception as e:
                         self.logger.error(e)
         
@@ -65,38 +67,39 @@ class BeikeErshoufangSimpleSpider(scrapy.Spider):
             subDistrict = response.url.split("/")[4]
         except Exception as e:
             self.logger.error(e)
-        houseSize = response.css('.leftContent > div > .clear > .fl > span::text').get()
+        houseSize = response.css('.content__title--hl::text').get()
         pageSize = int(int(houseSize) / 30) + 1
         self.logger.info("板块：" + subDistrict + "，房屋数量：" + str(houseSize) + "，房屋页数：" + str(pageSize))
         for i in range(1, (pageSize + 1)):   
-            subDistrictSubPageUrl = 'https://bj.ke.com/ershoufang/' + subDistrict + "/pg" + str(i) + "/"
-            #subDistrictSubPageUrl = 'https://bj.ke.com/ershoufang/yangzhuang1/pg10/'
+            subDistrictSubPageUrl = base_url + subDistrict + "/pg" + str(i) + "/"
             yield scrapy.Request(subDistrictSubPageUrl, self.subDistrictSubPage)
             # TODO 删了
             # return
     
     def subDistrictSubPage(self, response):
-        subDistrict = response.css('.leftContent > div > .clear > .fl > a::text').get().replace("二手房", "")
-        priceItems = response.css('.totalPrice2 > span')
-        communityItems = response.css('.flood a')
-        houseSimpleInfoItems = response.css('.houseInfo')
+        titleItems = response.css('.twoline')
+        priceItems = response.css('em')
+        communityItems = response.css('.content__list--item--des > a')
+        houseSimpleInfoItems = response.css('.content__list--item--des')
         index = 0
-        for eachHouseInfo in response.css('.title > .maidian-detail'):
-            houseUrl = eachHouseInfo.xpath("@href").extract_first()
-            houseUrlSplitResultList = houseUrl.split("/")
-            houseTitle = eachHouseInfo.xpath("@title").extract_first()
-            roomId = houseUrlSplitResultList[len(houseUrlSplitResultList) - 1].split(".")[0]
+        for eachHouseInfo in response.css('.content__list--item--title'):
             price = float(priceItems[index].xpath("text()").extract_first()) * 10000
-            community = communityItems[index].xpath("text()").extract_first()
-            areaList = houseSimpleInfoItems[index].get().split("|")
+            district = communityItems[3 * index].xpath("text()").get()
+            subDistrict = communityItems[1 + 3 * index].xpath("text()").get()
+            community = communityItems[2 + 3 * index].xpath("text()").get()
+            houseUrl = beike_url + titleItems[index].xpath("@href").extract_first()
+            houseUrlSplitResultList = houseUrl.split("/")
+            roomId = houseUrlSplitResultList[len(houseUrlSplitResultList) - 1].split(".")[0]
+            houseTitle = titleItems[index].xpath("text()").extract_first().strip()
+            areaList = houseSimpleInfoItems[index].get().split("<i>/</i>")
             area = 0
             for mayArea in areaList:
-                if ("平米" in mayArea):
-                    area = float(mayArea.strip().replace("平米", "").replace("\n", ""))
+                if ("㎡" in mayArea):
+                    area = float(mayArea.strip().replace("㎡", "").replace("\n", ""))
 
+            #self.logger.error("subDistrictSubPage price:%f, district:%s subDistrict:%s, community:%s, houseUrl:%s, houseTitle:%s, roomId:%s, areaList:%s, area:%f", price, district, subDistrict, community, houseUrl, houseTitle, roomId, areaList, area)
             if (area == 0):
                 self.logger.error("subDistrictSubPage area error areaList:%s", areaList)
-            self.logger.error("subDistrictSubPage area error areaList:%s", areaList)
             index = index + 1
             objectId = ""
             if (objectId == ""):
@@ -107,7 +110,7 @@ class BeikeErshoufangSimpleSpider(scrapy.Spider):
                     totalPrice = price,
                     area = area,
                     city = "beijing",
-                    district = "",
+                    district = district,
                     subDistrict = subDistrict,
                     community = community,
                     online = True,
